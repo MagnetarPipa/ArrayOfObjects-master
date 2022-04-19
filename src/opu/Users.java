b55b4e40ff852c40394591834417969a2219dbbc
MagnetarPipa <GITHUB you@example.com>
2022-04-19 20:36:39 +0300

    Todo's done

---
+++ src/opu/Users.java
@@ -0,0 +1,83 @@
+package opu;
+
+import java.time.LocalDate;
+
+public class Users {
+
+    private User[] users;
+    private int currentUserNumber;
+
+    // todo auto password change for users, registered more than DAYS days ago
+
+    public Users(final int usersNumber) {
+        this.users = new User[usersNumber];
+        currentUserNumber = -1;
+    }
+
+    public void changeUserPassword(final int user_num, final int days_ago) {
+
+        if (LocalDate.now().getDayOfMonth() - users[user_num].getRegistrationDate().getDayOfMonth() >= days_ago) {
+            users[user_num].setPassword(utilityPasswordMaker.generatePassword());
+        }
+    }
+
+    public User findOldestUser() {
+        User user1 = users[0];
+
+        for (final User user : users) {
+            if (user.getRegistrationDate().isBefore(user1.getRegistrationDate())) user1 = user;
+        }
+
+        return user1;
+    }
+
+    public void addUser(final User user) {
+        if (++currentUserNumber < users.length) {
+            users[currentUserNumber] = user;
+        }
+    }
+
+    private void delUserByNumber(final int number) {
+        final Users tempUsers = new Users(currentUserNumber);
+        tempUsers.currentUserNumber = currentUserNumber;
+
+        int newIndex = 0;
+        for (int i = 0; i <= currentUserNumber; i++) {
+            if (i == number) continue;
+            tempUsers.users[newIndex++] = users[i];
+        }
+        users = tempUsers.users;
+        currentUserNumber--;
+    }
+
+    public void removeUser(final int number) {
+        if (number < 0 || number >= currentUserNumber) {
+            return;
+        } else {
+            delUserByNumber(number);
+        }
+    }
+
+    public void removeUser(final String login) {
+        int number = 0;
+        for (final User user : users) {
+            if (user.getLogin().equalsIgnoreCase(login.strip())) {
+                delUserByNumber(number);
+
+                return;
+            }
+            number++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String result = "Users: " + System.lineSeparator();
+
+        for (final User user : users) {
+            result += user + System.lineSeparator();
+        }
+
+        return result;
+    }
+}
---
+++ src/opu/utilityPasswordMaker.java
@@ -0,0 +1,90 @@
+package opu;
+
+import java.util.Random;
+
+public class utilityPasswordMaker {
+
+    public static final int MIN_PASSWORD_LENGTH = 8;
+
+    public static boolean isCorrectPassword(final String password) {
+        // password validity check: at least 8 chars length, 1 UppercaseLetter, 1 lowerCase letter, 1 digit, 1 non AlphaOrDigit
+        boolean hasUppercaseLetter = false;
+        boolean hasLowerCaseLetter = false;
+        boolean hasDigit = false;
+        boolean hasNonAlphaOrDigit = false;
+        boolean result = false;
+
+        if (password.length() > MIN_PASSWORD_LENGTH) {
+            for (final Character character : password.toCharArray()) {
+                if (Character.isUpperCase(character)) {
+                    hasUppercaseLetter = true;
+                    break;
+                }
+            }
+            for (final Character character : password.toCharArray()) {
+                if (Character.isLowerCase(character)) {
+                    hasLowerCaseLetter = true;
+                    break;
+                }
+            }
+            for (final Character character : password.toCharArray()) {
+                if (Character.isDigit(character)) {
+                    hasDigit = true;
+                    break;
+                }
+            }
+            for (final Character character : password.toCharArray()) {
+                if (!Character.isAlphabetic(character) && !Character.isDigit(character)) {
+                    hasNonAlphaOrDigit = true;
+                    break;
+                }
+            }
+
+            if (hasUppercaseLetter && hasLowerCaseLetter && hasDigit && hasNonAlphaOrDigit) result = true;
+        }
+
+        return result;
+    }
+
+    public static String generatePassword() {
+        String password = "";
+
+        // password generation
+        final String chars = "!@#$%^&*()_+~`|\\?><";
+        final String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        final String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        final String digits = "0123456789";
+        Random random = new Random();
+
+        for (int i = 0; i < 2; i++) {
+            password += chars.charAt(random.nextInt(chars.length()));
+            password += upperCaseLetters.charAt(random.nextInt(upperCaseLetters.length()));
+            password += lowerCaseLetters.charAt(random.nextInt(lowerCaseLetters.length()));
+            password += digits.charAt(random.nextInt(digits.length()));
+        }
+
+
+        char[] symbols = password.toCharArray();
+        char tmp;
+        int index1, index2;
+
+        for (int i = 0; i < symbols.length; i++) {
+
+            index1 = (int) (Math.random() * symbols.length);
+            index2 = (int) (Math.random() * symbols.length);
+
+            tmp = symbols[index1];
+            symbols[index1] = symbols[index2];
+            symbols[index2] = tmp;
+        }
+
+        password = "";
+        for (int i = 0; i < symbols.length; i++) {
+            password += symbols[i];
+        }
+
+        // todo chars of the password MANY times to prevent analysis
+
+        return password;
+    }
+}
